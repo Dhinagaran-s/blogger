eea218ac7777db657b8eb10b19abf69884296eaf
Artem Derevianko <artem.d.6@gmail.com>
2013-05-07 16:56:05 -0400

    commit day 2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def new
+    @article = Article.new    
+  end
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
