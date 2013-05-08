efd2d79dab963876153624247c280d3f5bdc1fb0
Artem Derevianko <artem.d.6@gmail.com>
2013-05-08 17:14:08 -0400

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Blogger::Application.routes.draw do
 
   root :to => 'articles#index'
 
-  resources :articles
-
+  resources :articles do
+    resources :comments
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
